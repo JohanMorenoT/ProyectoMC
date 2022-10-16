ccb051ccd930ff5a2e0ae502264ddb37e6349673
JohanMorenoT <114170704+JohanMorenoT@users.noreply.github.com>
2022-10-15 20:44:43 -0500

    Poyecto base de mi cultivo...

---
+++ ProyectoMC/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProyectoMC project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoMC.settings')
+
+application = get_asgi_application()
